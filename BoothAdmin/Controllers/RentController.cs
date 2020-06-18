6cb04ec3a32d579ac4795a75cf7066b97289cd84
朱佳龙 <641666169@qq.com>
2020-06-18 21:39:28 +0800

    租赁

--- BoothAdmin/Controllers/RentController.cs
+++ BoothAdmin/Controllers/RentController.cs
@@ -2,39 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BoothAdmin.Controllers
 {
-    public class RentController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RentController : ControllerBase
     {
-        public IActionResult InsertRent()
-        {
-            return View();
-        }
-        /// <summary>
-        /// 竞拍单详情
-        /// </summary>
-        /// <returns></returns>
-        public IActionResult RentShow()
-        {
-            return View();
-        }
-        /// <summary>
-        /// 反填订单详情
-        /// </summary>
-        /// <returns></returns>
-        public IActionResult Update()
-        {
-            return View();
-        }
-        /// <summary>
-        /// 合同管理
-        /// </summary>
-        /// <returns></returns>
-        public IActionResult ConTastShow()
-        {
-            return View();
-        }
     }
 }
