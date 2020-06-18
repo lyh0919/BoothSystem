200bd4bf9195e49870c651bbc938070fd4a36e76
1103914244@qq.com <1103914244@qq.com>
2020-06-18 08:56:41 +0800

    city

---
+++ BoothModel/Models/City.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoothModel.Models
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public int PId { get; set; }
+        public string CityName { get; set; }
+        public int CType { get; set; }
+    }
+}
