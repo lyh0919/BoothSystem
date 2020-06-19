f4e5f0e287ed707db555d7508da00878261a6b38
1660450577@qq.com <1660450577@qq.com>
2020-06-19 21:01:32 +0800

    添加+显示

--- BoothAdmin/Controllers/MarketController.cs
+++ BoothAdmin/Controllers/MarketController.cs
@@ -12,29 +12,14 @@ namespace BoothAdmin.Controllers
 {
     public class MarketController : Controller
     {
-        //private IMarketBll _marketBll;
-        //public MarketController(IMarketBll marketBll)
-        //{
-        //    this._marketBll = marketBll;
-        //}
+     
         public IActionResult Index()
         {
-            //string url = "http://localhost:52229/api/Market/ShowMarket";
-            //HttpClient client = new HttpClient();
-
-            //HttpResponseMessage httpResponse = client.GetAsync(url).Result;
-            //string s = httpResponse.Content.ReadAsStringAsync().Result;
-            //List<MarketInfo> list = JsonConvert.DeserializeObject<List<MarketInfo>>(s);
-            //List<MarketInfo> list = _marketBll.ShowMarket(page,limit);
-            //int PageCount = (list == null || list.Count == 0) ? 0 : list.Count;
-            //int c = (int)Math.Ceiling((decimal)PageCount / limit);
-            //ViewBag.parper = (page <= 1) ? 1 : page - 1;
-            //ViewBag.pagenext = page >= c ? c : page + 1;
-            //ViewBag.pahelast = c;
-            //return Json(new LayUi { code = "0", msg = "", count = PageCount.ToString(), data = list });
+           
             return View();
            
         }
+        //通过调用api做显示
         public ActionResult SelectMarket(int page=1,int limit=10)
         {
 
@@ -51,9 +36,7 @@ namespace BoothAdmin.Controllers
             ViewBag.pagenext = page >= c ? c : page + 1;
             ViewBag.pahelast = c;
             return Json(new LayUi { code = "0", msg = "", count = PageCount.ToString(), data = list });
-            //List<MarketInfo> list = JsonConvert.DeserializeObject<List<MarketInfo>>(s);
-            //return View(list);
-            //return View();
+            
         }
 
 
@@ -63,7 +46,14 @@ namespace BoothAdmin.Controllers
             return View();
         }
 
-       public IActionResult UptMarkert()
+       public IActionResult UpdateMarket(object id)
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult DelMarket(Object id)
         {
             return View();
         }
